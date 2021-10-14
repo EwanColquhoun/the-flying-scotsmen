a116e10c94a12d1e6baa5b7ebf2584632dcc6ead
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-10-14 20:57:37 +0000

    Set up base.html as template. Add basic JS for sidebar. Add sidebar.

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from .models import Booking, Aircraft, Slot
+# from .forms import CommentForm
+# from django.http import HttpResponseRedirect
+# from django.contrib import messages
 
-# Create your views here.
+
+# class BookingList(generic.ListView):
+#     model = Aircraft
+#     queryset = Aircraft.objects.all()
+#     template_name = 'index.html'
+
+
+class CalendarDisplay(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'booking/index.html',      
+        )
