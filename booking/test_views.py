24ed799166fbe99bf3d4fb8a4206e5cfedb40a20
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-12-16 10:48:34 +0000

    update requirements. Attempt to test the edit view to edit a booking.

--- booking/test_views.py
+++ booking/test_views.py
@@ -20,6 +20,7 @@ class TestViews(TestCase):
         self.slot = Slot.objects.create(slot= '0700-0900', start= '07:00', duration= '02:00')
         self.slot2 = Slot.objects.create(slot= '0900-1100', start= '09:00', duration= '02:00')
         self.aircraft = Aircraft.objects.create(reg= 'G-TEST', desc= 'form test')
+        self.factory = RequestFactory()
         # self.test_booking = Booking.objects.create(date= '2024-01-01', slot= self.slot, aircraft= self.aircraft, )
 
     def test_get_home_page(self):
@@ -43,75 +44,30 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'booking/contact.html')
     
     def test_get_edit_booking_page(self):
-        test_booking = Booking.objects.create(date= '2024-01-01', slot= self.slot, aircraft= self.aircraft, )
+        test_booking = Booking.objects.create(date= '2024-01-01', slot=self.slot, aircraft=self.aircraft, )
         response = self.client.get(f'/edit/{test_booking.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'booking/edit_booking.html')
 
     def test_can_make_booking(self):
-        test_booking = Booking.objects.create(date= '2024-01-01', slot= self.slot, aircraft= self.aircraft, )
-        # response = self.client.post('/bookings/', {'date': '2024-01-01', 'slot': '0', 'aircraft': 'G-TEST',})
+        test_booking = Booking.objects.create(date= '2024-01-01', slot=self.slot, aircraft=self.aircraft, )
         existing_booking = Booking.objects.filter(id=test_booking.id)
         self.assertEqual(len(existing_booking), 1)
 
     def test_can_delete_booking(self):
-        test_booking = Booking.objects.create(date= '2024-01-01', slot= self.slot, aircraft= self.aircraft, )
+        test_booking = Booking.objects.create(date= '2024-01-01', slot=self.slot, aircraft=self.aircraft, )
         response = self.client.get(f'/delete/{test_booking.id}')
         self.assertRedirects(response, '/bookings/')
         existing_booking = Booking.objects.filter(id=test_booking.id)
         self.assertEqual(len(existing_booking), 0)
 
-    def test_can_edit_booking(self):
-        test_booking = Booking.objects.create(date= '2024-01-01', slot= self.slot, aircraft= self.aircraft, )
-        response = self.client.post(
-            reverse('edit_booking', kwargs={'booking_id': test_booking.id}), 
-            {'date': '2024-02-02', 'slot': '0', 'aircraft': 'G-TEST',})
-     
-        # self.assertEqual(response.status_code, 302)
-
-        test_booking.refresh_from_db()
-        self.assertEqual(test_booking.date, '2024-02-02')
-
-
-# below tests are experiments to get a test for if showing if the edit page redirects to bookings (EDIT SUCCESS) or edit page(EDIT FAILED). 
-
-
-# class TestEditView(TestCase):
-
-#     def setUp(self):
-#         self.user = CustomUser.objects.create(username='testUser', email='test@email.com', password='')
-#         self.user.set_password('secret')
-#         self.user.save()
-#         self.client = Client()
-#         self.client.login(username='testUser', password='secret')
-#         self.slot = Slot.objects.create(slot= '0700-0900', start= '07:00', duration= '02:00')
-#         self.slot2 = Slot.objects.create(slot= '0900-1100', start= '09:00', duration= '02:00')
-#         self.aircraft = Aircraft.objects.create(reg= 'G-TEST', desc= 'form test')
-
-#     def test_edit_view(self):
-#         test_booking = Booking.objects.create(date='2024-01-01', slot=self.slot, aircraft=self.aircraft, )
-#         request = RequestFactory().post(f'/edit/{test_booking.id}', {'date': '2024-01-01', 'slot': '0900-1100', 'aircraft': 'G-TEST',})
-#         print(request)
-#         view = EditDisplay()
-#         view.setup(request)
-#         view.post(request, test_booking.id)
-#         self.assertRedirects(request, '/bookings/')
-
-
     # def test_can_edit_booking(self):
-    #     data = {'date': '2024-01-01', 'slot': '0900-1100', 'aircraft': 'G-TEST',}
+    #     data={'username': self.client, 'date': '2024-02-02', 'slot': '0', 'aircraft': 'G-TEST',}
     #     test_booking = Booking.objects.create(date='2024-01-01', slot=self.slot, aircraft=self.aircraft, )
-    #     print(test_booking.slot)
-    #     # response = self.client.get(f'/edit/{test_booking.id}')
-    #     response = self.client.post(f'/edit/{test_booking.id}', data=data)
-    #     print(response)
-    #     # up_booking = get_object_or_404(Booking, id=test_booking.id)
-    #     print(test_booking.slot)
-    #     self.assertRedirects(response, '/bookings/')
-    #     # self.assertEqual(new_response.status_code, 200)
-    #     # updated_booking = Booking.objects.get(id= test_booking.id)
-    #     # print(test_booking.slot)
-    #     # print(updated_booking.slot)
-    #     # self.assertFalse(updated_booking.slot, self.slot2)
-
-
+    #     request = self.client.post('/edit/{}'.format(test_booking.id), data=data)
+    #     request.user = self.client
+    #     test_booking.refresh_from_db()
+    #     updated_booking = Booking.objects.filter(date='2024-02-02', slot='1')
+    #     self.assertFalse(len(updated_booking), 0)
+    #     self.assertEqual(request.status_code, 302)
+    #     self.assertRedirects(request, '/bookings/')
