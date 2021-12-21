4c026d899b5a24e8d1aa213c3728fdb637194e56
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-12-21 15:19:43 +0000

    Deletemodal test works.

--- static/script/script.js
+++ static/script/script.js
@@ -1,42 +1,49 @@
 // Message Timeout
-function alerts(){
-  setTimeout(function() {
+function alerts() {
+  setTimeout(function () {
     let messages = document.getElementById('msg')
     let alert = new bootstrap.Alert(messages);
-    if (messages){
+    if (messages) {
       messages.classList.remove('show');
     };
-}, 5000);
+  }, 5000);
 }
 
 // signup forms
-signupForms = function(){
-  console.log('signup')
+signupForms = function () {
   document.getElementById("signup-form").submit();
   document.getElementById("signup-form2").submit();
 }
 // Sidebar functions
-function sidebar(){
+function sidebar() {
   let sidebar = document.querySelector(".sidebar");
   let closeBtn = document.querySelector("#btn");
-  
-    closeBtn.addEventListener("click", function open(){
-      sidebar.classList.toggle("open");
-    });
-}
+
+  closeBtn.addEventListener("click", function open() {
+    sidebar.classList.toggle("open");
+    if (sidebar.classList.contains('open')) {
+      return true;
+    } else {
+      return false
+    }
+  });
+};
+
 
 // Calendar functions
-function deleteModal(){
+function deleteModal() {
   // Gets the delete modal working
+  console.log("DELETE MODEL IS BEING CALLED BY JEST")
   const modal_buttons = document.getElementById('delete-modal-buttons')
   const delete_button = document.querySelectorAll('.delete_button')
 
-  if (delete_button.length !== 0){
+  if (delete_button.length !== 0) {
     delete_button.forEach((button) => {
-      button.addEventListener('click', function() {
+      button.addEventListener('click', function () {
+        console.log('JEST CLICKED!')
         const booking_id = button.getAttribute('name')
         let calbut = button.getAttribute('data-ref')
-        if (calbut === '0'){
+        if (calbut === '0') {
           modal_buttons.innerHTML = `
           <button type="button" class="btn btn-secondary" data-ref="0" data-bs-dismiss="modal">Close</button>
           <a href="/delete/${booking_id}" class="btn btn-back">DELETE</a>
@@ -45,14 +52,14 @@ function deleteModal(){
           modal_buttons.innerHTML = `
           <button type="button" class="btn btn-secondary" data-ref="1" data-bs-dismiss="modal">Close</button>
           <a href="/delete_calendar_booking/${booking_id}" class="btn btn-back">DELETE</a>
-          `     
+          `
         }
       });
     })
   }
 }
 
-function today(){
+function today() {
   // Highlights today on the calendar
   const dates = document.querySelectorAll('.date')
   d = new Date();
@@ -61,144 +68,208 @@ function today(){
   now = `${month}_${day_of_the_month}`
   getToday()
 
- function getToday() {
+  function getToday() {
     dates.forEach((date) => {
       let today = date.getAttribute('name');
-      if (today == now){
+      if (today == now) {
         date.setAttribute('id', 'today')
       }
     })
   }
 }
 
-function calendarBookingModal(){
+function calendarBookingModal() {
   // Activates the calendar booking modal
   const dates = document.querySelectorAll('.date')
 
   const modal = document.getElementById('myModal')
-  const noBookings = document.getElementById('no-bookings-text') 
+  const noBookings = document.getElementById('no-bookings-text')
   const bookingTable = document.getElementById('booking-table')
 
   dates.forEach((date) => {
-    date.addEventListener('click', function() {
+    date.addEventListener('click', function () {
       let today = date.getAttribute('name');
       let bookings = document.querySelectorAll(`.booking_${today}`)
 
-      if (bookings.length === 0 ){
+      if (bookings.length === 0) {
         bookingTable.classList.replace('show', 'hide')
         noBookings.classList.replace('hide', 'show')
       } else {
         noBookings.classList.replace('show', 'hide')
         bookingTable.classList.replace('hide', 'show')
-        for (let a = 0; a <= bookings.length; a++){
+        for (let a = 0; a <= bookings.length; a++) {
           bookings[a].classList.replace('hide', 'show')
           modal.addEventListener('hidden.bs.modal', function () {
             bookings[a].classList.replace('show', 'hide')
-                
-              });
-            } 
+
+          });
+        }
       }
-    }); 
+    });
   });
 }
 
-function calendar(){
+function calendar() {
   deleteModal()
   today()
   calendarBookingModal()
 }
 
 // Map with Markers
-function initMap(){
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 52.3308, lng: 1.6851},
+    center: {
+      lat: 52.3308,
+      lng: 1.6851
+    },
     zoom: 4.5
   });
 }
 
-function googleMapApi(){
+function googleMapApi() {
   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var locations = [
-    { lat: 55.9508, lng: -3.3615},
-    { lat: 58.2139, lng: -6.3219},
-    { lat: 57.2037, lng: -2.2002},
-    { lat: 57.3406, lng: -5.6491},
-    { lat: 53.8981, lng: -0.3660},
-    { lat: 55.0372, lng: -8.3414},
-    { lat: 50.9660, lng: -2.1572},
-    { lat: 50.1024, lng: -5.6681},
-    { lat: 50.9557, lng: 0.9335},
-    { lat: 47.4072, lng: 8.6396},
-    { lat: 52.4825, lng: 13.3891},
-    { lat: 48.5897, lng: -2.0758},
-    { lat: 51.9555, lng: 4.4399},
-    { lat: 48.9812, lng: 6.2437},
-    { lat: 48.8335, lng: 3.0056},
-    { lat: 56.4391, lng: -3.3714},
-    { lat: 55.6836, lng: -6.2483},
+  var locations = [{
+      lat: 55.9508,
+      lng: -3.3615
+    },
+    {
+      lat: 58.2139,
+      lng: -6.3219
+    },
+    {
+      lat: 57.2037,
+      lng: -2.2002
+    },
+    {
+      lat: 57.3406,
+      lng: -5.6491
+    },
+    {
+      lat: 53.8981,
+      lng: -0.3660
+    },
+    {
+      lat: 55.0372,
+      lng: -8.3414
+    },
+    {
+      lat: 50.9660,
+      lng: -2.1572
+    },
+    {
+      lat: 50.1024,
+      lng: -5.6681
+    },
+    {
+      lat: 50.9557,
+      lng: 0.9335
+    },
+    {
+      lat: 47.4072,
+      lng: 8.6396
+    },
+    {
+      lat: 52.4825,
+      lng: 13.3891
+    },
+    {
+      lat: 48.5897,
+      lng: -2.0758
+    },
+    {
+      lat: 51.9555,
+      lng: 4.4399
+    },
+    {
+      lat: 48.9812,
+      lng: 6.2437
+    },
+    {
+      lat: 48.8335,
+      lng: 3.0056
+    },
+    {
+      lat: 56.4391,
+      lng: -3.3714
+    },
+    {
+      lat: 55.6836,
+      lng: -6.2483
+    },
   ]
 
-  var markers = locations.map(function(location, i) {
+  var markers = locations.map(function (location, i) {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length]
     });
   });
   var markerCluster = new MarkerClusterer(map, markers, {
-    imagePath:
-    "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
 
 // flatpickr
-function flatpickrInit(){
+function flatpickrInit() {
   const myInput = document.querySelector("#id_date");
   const fp = flatpickr(myInput, {
     minDate: "today"
-  });  // flatpickr
+  }); // flatpickr
 }
 
 // provides feedback if passwords match
-function passwordMatch(){
+
+function passwordMatch() {
 
   let password1 = document.getElementById('id_password1')
   let password2 = document.getElementById('id_password2')
 
-  password2.addEventListener("input", function(){
-    if (password2.value === password1.value){
+  password2.addEventListener("input", function pMatch() {
+    if (password2.value === password1.value) {
       password2.classList.add('matched')
       password1.classList.add('matched')
+      return true;
     } else {
       password2.classList.remove('matched')
       password1.classList.remove('matched')
+      return false;
     }
   });
-
-}
+};
 
 // Site initialisation
+
 let password = document.querySelectorAll('#id_password1')
 let date_input = document.querySelectorAll('#id_date')
 let map_div = document.querySelectorAll('#map')
 let calendar_page = document.querySelectorAll('#calendar-page')
 let message_container = document.querySelector('#message-container')
 
-
-window.addEventListener('load', ()=> {
+function setup() {
   alerts()
   sidebar()
-  if (password.length >= 1){
-      passwordMatch();
-  } else if (calendar_page.length >= 1){
-      calendar()
-  } else if (date_input.length && map_div.length >= 1){
-      flatpickrInit();
-      deleteModal()
-      initMap()
-      googleMapApi()
-  } else if (date_input.length >= 1){
-      flatpickrInit();
+  if (password.length >= 1) {
+    passwordMatch();
+    return 'password'
+  } else if (calendar_page.length >= 1) {
+    calendar()
+    return 'calendar'
+  } else if (date_input.length && map_div.length >= 1) {
+    flatpickrInit();
+    deleteModal()
+    initMap()
+    googleMapApi()
+    return 'contact'
+  } else if (date_input.length >= 1) {
+    flatpickrInit();
+    return 'bookings'
   };
-});
+};
 
-// module.exports = sidebar();
+const start = (
+  window.addEventListener('load', () => {
+    setup()
+  })
+)
+
+module.exports = { deleteModal };
--- static/script/tests/script.test.js
+++ static/script/tests/script.test.js
@@ -2,7 +2,31 @@
  * @jest-environment jsdom
 */
 
-const myScript = require("/workspace/the-flying-scotsmen/static/script/script.js");
+const start = require("/workspace/the-flying-scotsmen/static/script/script.js");
+const setup = require("/workspace/the-flying-scotsmen/static/script/script.js");
+const deleteModal = require("/workspace/the-flying-scotsmen/static/script/script.js");
+
+// wanted this to show that when the passwords matched either true or false is returned from the function----------------------------------
+
+// describe("Password match test", () => {
+//     beforeAll(() => {
+//         document.body.innerHTML = `<input type="password" name="password1" placeholder="Password" autocomplete="new-password" required="" id="id_password1">
+//         <input type="password" name="password2" placeholder="Password Again" autocomplete="new-password" required="" id="id_password2">`
+//     })
+
+//     test("Check passwords match", () => {
+//         // let password1 = document.getElementById('id_password1')
+//         // let password2 = document.getElementById('id_password2')
+//         // password1.value = 'testpassword'
+//         // password2.value = 'testpassword'
+//         // const spy = jest.spyOn(start, 'passwordMatch')
+//         // const match = start.passwordMatch();
+       
+//         // expect(spy).toHaveBeenCalled();
+//         // expect(match).toBe(true)
+//     })
+// })
+
 
 describe("Base.html tests", () => {
 
@@ -22,54 +46,123 @@ describe("Base.html tests", () => {
         expect(document.querySelectorAll("#btn").length).toBe(1);
     });
 
+// wanted these to show that the button opens the sidebar ---------------------
+
     // test("sidebar.open should exist", () => {
-    //     sidebar();
+    //     const spy = jest.spyOn(setup, "sidebar" );
+    //     const side = setup.sidebar();
+    //     const addEvt = new Event('click');
+    //     document.dispatchEvent(addEvt);
     //     expect(document.getElementById("sidebar").classList).toContain("open");
+    //     expect(spy).toHaveBeenCalled();
+    //     expect(side).toBe(true)
     // });
 
 
-    test('resize to test the navbar exists', () => {
-        // render(<Component />);
-        const resizeWindow = (width, height) => {
-            window.innerWidth = width
-            window.innerHeight = height
-            window.dispatchEvent(new Event('resize'))
-          }
-        resizeWindow(1200, 1200)
+    // test('resize to test the navbar exists', () => {
+    //     // render(<Component />);
+    //     const resizeWindow = (width, height) => {
+    //         window.innerWidth = width
+    //         window.innerHeight = height
+    //         window.dispatchEvent(new Event('resize'))
+    //       }
+    //     resizeWindow(1200, 1200)
     
-        const element = document.getElementById('navbar');
-        const styles = getComputedStyle(element);
+    //     const element = document.getElementById('navbar');
+    //     const styles = getComputedStyle(element);
     
-        expect(styles.display).toBe('block');
-    })
+    //     expect(styles.display).toBe('block');
+    // })
 });
 
-describe("Calendar tests", () => {
+// these all pass but dont test the js..----------------------
+describe("Bookings tests", () => {
     beforeAll(() => {
         let fs = require("fs");
-        let fileContents = fs.readFileSync("templates/booking/calendar.html", "utf-8");
+        let fileContents = fs.readFileSync("templates/booking/bookings.html", "utf-8");
         document.open();
         document.write(fileContents);
         document.close();
     });
-    
-    test("Test today is today", () => {
-        const date = document.querySelectorAll(".date")
-        console.log(date.length)
-        expect(date).toBe(1)
-    })
 
-    test("Test today is highlighted", () => {
-            
-        const element = document.getElementById('today');
-        const styles = getComputedStyle(element);
-    
-        expect(styles.backgroundColor).toBe('#0065bd');
-    })
-    test("h1 should exist", () => {
-        expect(document.getElementsByTagName("h1").length).toBe(1);
+    test("booking form should exist", () => {
+        expect(document.getElementById("booking-form").length).toBe(1);
     });
-    test("p should exist", () => {
-        expect(document.getElementsByTagName("p").length).toBe(5);
+    test("bookings table should exist", () => {
+        expect(document.getElementsByTagName("table").length).toBe(1);
+    });
+    test("delete booking modal not shown", () => {
+        expect(document.getElementById('staticBackdrop')).toBeNull();
+    })
+});
+
+describe("booking with bookings tests", ()=> {
+    beforeAll(() => {
+        document.body.innerHTML= `
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title scots" id="staticBackdropLabel">Delete Booking</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="delete-modal-body d-flex justify-content-center mt-4 mb-4">
+                        <p><i class="fas fa-exclamation-triangle"></i> Are you sure you want to delete this booking? <i class="fas fa-exclamation-triangle"></i></p>
+                    </div>
+                    <div class="modal-footer" id="delete-modal-buttons">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <a aria-label="Delete" href="/delete_calendar_booking/{{ booking.id }}" class="btn btn-back delete-button">DELETE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <tr class="booking-row approved">
+        <td>08/12</td>
+        <td>MAINT</td>
+        <td>G-BSAI</td>
+        <td class="notes"></td>
+        <td>No</td>
+        <td><span><a aria-label="Edit" href="/edit/2"><i class="far fa-edit edit-icon green" aria-hidden="true"></i></a></span></td>
+        <td>
+            <span><a aria-label="Delete" name="2" class="delete_button" data-ref="0" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <i class="far fa-times-circle cancel-icon red" aria-hidden="true"></i></a>
+            </span>
+        </td>
+    </tr>`
+        })
+
+    test("delete booking if activated should pass", () => {
+    
+        const spy = jest.spyOn(deleteModal, 'deleteModal')
+        deleteModal.deleteModal()
+        const delete_button = document.querySelectorAll('.delete_button')
+        delete_button.forEach(button => {
+            button.click()
+        });
+        // expect(spy).toHaveBeenCalled();
+        expect(deleteModal.deleteModal).toBeCalled();
+        let modals= document.querySelectorAll('#staticBackdrop')
+        expect(modals.length).toEqual(1);
+
+    })
+})
+
+// all pass but again dont really the js..-----------------------------
+describe("Contact tests", () => {
+    beforeAll(() => {
+        let fs = require("fs");
+        let fileContents = fs.readFileSync("templates/booking/contact.html", "utf-8");
+        document.open();
+        document.write(fileContents);
+        document.close();
+    });
+
+    test("contact form should exist", () => {
+        expect(document.getElementById("contact-form").length).toBe(1);
+    });
+    test("email has correct path", () => {
+        let email = document.getElementById("email-text")
+        let attr = email.getAttribute('href')
+        expect(attr).toEqual("mailto:theflyingscotsmen.booking@gmail.com");
     });
 });
