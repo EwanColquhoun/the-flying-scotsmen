cb1d56a78362f8151ebfd554e32b338ca2539d3d
EwanColquhoun <ewancolquhoun@hotmail.com>
2022-01-05 19:49:19 +0000

    Finish automated testing suites

--- booking/test_email.py
+++ booking/test_email.py
@@ -1,28 +1,40 @@
 from django.test import TestCase
-# from django.core import mail
 from django.test.client import Client
 from accounts.models import CustomUser
 from .models import Slot, Booking, Contact, Aircraft
-# from .forms import ContactForm
-from .email import send_contact_email_to_admin, send_email_to_admin, send_register_email_to_admin
-# import smtplib, ssl
-# import os
+from .email import (
+    send_email_to_admin,
+    send_contact_email_to_admin,
+    send_register_email_to_admin)
 
 
 class TestEmailUtil(TestCase):
 
     def setUp(self):
-        self.user = CustomUser.objects.create(username='testUser', email='test@email.com', password='')
+        self.user = CustomUser.objects.create(
+            username='testUser',
+            email='test@email.com',
+            password='')
         self.user.set_password('secret')
         self.user.save()
         self.client = Client()
         self.client.login(username='testUser', password='secret')
-        self.slot = Slot.objects.create(slot='0700-0900', start='07:00', duration='02:00')
+        self.slot = Slot.objects.create(
+            slot='0700-0900',
+            start='07:00',
+            duration='02:00')
         self.aircraft = Aircraft.objects.create(reg='G-TEST', desc='form test')
-        self.booking = Booking.objects.create(date='2024-01-01', slot=self.slot, aircraft=self.aircraft, )
+        self.booking = Booking.objects.create(
+            date='2024-01-01',
+            slot=self.slot,
+            aircraft=self.aircraft, )
 
     def test_send_contact_email(self):
-        contact = Contact.objects.create(name='Joe', telephone='123456789', email='joe@tfs.com', message='Test contact message')
+        contact = Contact.objects.create(
+            name='Joe',
+            telephone='123456789',
+            email='joe@tfs.com',
+            message='Test contact message')
         send_contact_email_to_admin(contact)
         only_contact = Contact.objects.filter(name='Joe')
         self.assertEqual(len(only_contact), 1)
@@ -40,26 +52,6 @@ class TestEmailUtil(TestCase):
         user.last_name = 'Awesome'
         user.email = 'ap@tfs.com'
         user.message = 'Please accept me'
-        
         send_register_email_to_admin(user)
         name = self.user.username
         self.assertEqual(name, 'testUser')
-
-# below test passes but only tests that the contact data is sent to below email- not really a test for the email.py
-
-# class EmailTest(TestCase):
-#     def test_send_contact_email(self):
-#         # Send message.
-#         contact = Contact.objects.create(name='Joe', telephone='123456789', email='joe@tfs.com', message='Test contact message')
-#         mail.send_mail(
-#             'Contact Request', contact.message,
-#             contact.email, ['Joe@example.com'],
-#             fail_silently=False,
-#         )
-
-#         # Test that one message has been sent.
-#         self.assertEqual(len(mail.outbox), 1)
-
-#         # Verify that the subject of the first message is correct.
-#         self.assertEqual(mail.outbox[0].subject, 'Contact Request')
-#         self.assertEqual(mail.outbox[0].body, 'Test contact message')
--- booking/test_forms.py
+++ booking/test_forms.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.test.client import Client
-from django.conf import settings
 from accounts.models import CustomUser
 from booking.models import Slot, Aircraft
 from .forms import BookingForm, ContactForm
@@ -15,53 +14,79 @@ class TestContactForm(TestCase):
         self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_telephone_is_required(self):
-        form = ContactForm({'name': 'John', 'telephone':''})
+        form = ContactForm({'name': 'John', 'telephone': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('telephone', form.errors.keys())
         self.assertEqual(form.errors['telephone'][0], 'This field is required.')
-    
+
     def test_email_is_required(self):
-        form = ContactForm({'name': 'John', 'telephone':'12345678910', 'email':''})
+        form = ContactForm({
+            'name': 'John',
+            'telephone': '12345678910',
+            'email': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('email', form.errors.keys())
         self.assertEqual(form.errors['email'][0], 'This field is required.')
 
     def test_message_is_required(self):
-        form = ContactForm({'name': 'John', 'telephone':'12345678910', 'email':'john@john.com', 'message':''})
+        form = ContactForm({
+            'name': 'John',
+            'telephone': '12345678910',
+            'email': 'john@john.com',
+            'message': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('message', form.errors.keys())
         self.assertEqual(form.errors['message'][0], 'This field is required.')
 
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ContactForm()
-        self.assertEqual(form.Meta.fields, ('name', 'telephone', 'email', 'message'))
+        self.assertEqual(form.Meta.fields, (
+            'name',
+            'telephone',
+            'email',
+            'message'))
 
 
 class TestBookingForm(TestCase):
 
-    def setUp(self): 
-        self.user = CustomUser.objects.create(username='testUser', email='test@email.com', password='')
+    def setUp(self):
+        self.user = CustomUser.objects.create(
+            username='testUser',
+            email='test@email.com',
+            password='')
         self.user.set_password('secret')
         self.user.save()
         self.client = Client()
         self.client.login(username='testUser', password='secret')
-        self.slot = Slot.objects.create(slot= '0700-0900', start= '07:00', duration= '02:00')
-        self.aircraft = Aircraft.objects.create(reg= 'G-TEST', desc= 'form test')
+        self.slot = Slot.objects.create(
+            slot='0700-0900',
+            start='07:00',
+            duration='02:00')
+        self.aircraft = Aircraft.objects.create(reg='G-TEST', desc='form test')
 
     def test_slot_is_required(self):
-        form = BookingForm(user= Client, data= {'date': '2024/01/01', 'slot': '', 'aircraft': '0' })
+        form = BookingForm(user=Client, data={
+            'date': '2024/01/01',
+            'slot': '',
+            'aircraft': '0'})
         self.assertFalse(form.is_valid())
         self.assertIn('slot', form.errors.keys())
         self.assertEqual(form.errors['slot'][0], 'This field is required.')
 
     def test_aircraft_is_required(self):
-        form = BookingForm(user= Client, data= {'date': '2024/01/01', 'slot': '0', 'aircraft': '' })
+        form = BookingForm(user=Client, data={
+            'date': '2024/01/01',
+            'slot': '0',
+            'aircraft': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('aircraft', form.errors.keys())
         self.assertEqual(form.errors['aircraft'][0], 'This field is required.')
 
     def test_date_is_required(self):
-        form = BookingForm(user= Client, data= {'date': '', 'slot': '0', 'aircraft': '0' })
+        form = BookingForm(user=Client, data={
+            'date': '',
+            'slot': '0',
+            'aircraft': '0'})
         self.assertFalse(form.is_valid())
         self.assertIn('date', form.errors.keys())
         self.assertEqual(form.errors['date'][0], 'This field is required.')
